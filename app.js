0699809eed12bbdc48218a0acb355db8122e4c46
WeraChernevich <138397007+WeraChernevich@users.noreply.github.com>
2023-09-11 17:24:09 +0300

    Profit
    1

---
+++ app.js
@@ -0,0 +1,110 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // loop: true,
+  parallax: true,
+  speed: 1000,
+
+  keyboard: {
+    enabled: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.slider-controls__count',
+    type: 'fraction',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// document.querySelector('.nav-burger').addEventListener('click', function () {
+//   this.classList.toggle('active');
+//   document.querySelector('.mobile-nav').classList.toggle('open');
+//   document.querySelector('.mobile-nav-overlay').classList.toggle('open');
+//   document.querySelector('.mobile-nav__nav').classList.toggle('active');
+//   document.querySelector('.mobile-nav__nav-burger').classList.toggle('active');
+
+// });
+
+var burgerOpen = document.querySelector('.nav-burger');
+burgerOpen.addEventListener('click', openOnClick);
+
+function openOnClick() {
+  burgerOpen.classList.toggle('active');
+  document.querySelector('.mobile-nav').classList.toggle('open');
+  document.querySelector('.mobile-nav-overlay').classList.toggle('open');
+  document.querySelector('.mobile-nav__nav').classList.toggle('active');
+  document.querySelector('.mobile-nav__nav-burger').classList.toggle('active');
+
+  var burgerClose = document.querySelector('.mobile-nav__nav-burger.active');
+  burgerClose.addEventListener('click', closeOnClick);
+
+  function closeOnClick() {
+    burgerClose.classList.remove('active');
+    document.querySelector('.nav-burger.active').classList.remove('active');
+    document.querySelector('.mobile-nav.open').classList.remove('open');
+    document.querySelector('.mobile-nav-overlay.open').classList.remove('open');
+    document.querySelector('.mobile-nav__nav.active').classList.remove('active');
+    document.querySelector('.mobile-nav__nav-burger.active').classList.remove('active');
+
+
+  };
+ };
+
+
+
+
+
+
+
+
+// document.querySelector('.nav-burger').addEventListener('click', function () {
+//   this.classList.toggle('active');
+//   document.querySelector('.mobile-nav').classList.toggle('open');
+//   document.querySelector('.mobile-nav-overlay').classList.toggle('open');
+//   document.querySelector('.mobile-nav__nav').classList.toggle('active');
+// });
+//   document.querySelector('.mobile-nav__nav-burger').classList.toggle('active');
+
+//   const navBtnClose = document.querySelector('.mobile-nav__nav-burger');
+//   navBtnClose.onclick = ReturnAll;
+
+//   function ReturnAll() {return};
+
+// });
+
+
+// const menuBtn = document.querySelector(".nav-burger");
+// const menuItems = document.querySelector(".mobile-nav");
+// const menuScreen = document.querySelectorAll(".mobile-nav-overlay");
+// const menuItem = document.querySelector(".mobile-nav__nav-burger");
+
+
+// function toggle() {
+//   menuBtn.classList.toggle("open");
+//   menuItems.classList.toggle("open");
+//   menuScreen.classList.toggle("open");
+// }
+
+
+// menuBtn.addEventListener("click", () => {
+//   toggle();
+// })
+
+
+// menuItem.forEach(item => {
+//   item.addEventListener("click", () => {
+//     if (menuBtn.classList.contains("open")) {
+//       toggle();
+//     }
+//   });
+// });
